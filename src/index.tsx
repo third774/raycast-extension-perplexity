61b5a421f3f36278a1c08e83317496d03435884c
Kevin Kipp <kevin.kipp@gmail.com>
2023-11-04 17:58:14 -0500

    Add focus dropdown

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,15 @@ import { Action, ActionPanel, Form, open, popToRoot } from "@raycast/api";
 type Values = {
   query: string;
   copilot: boolean;
+  focus: string;
 };
 
 export default function Command() {
-  function handleSubmit({ query: q, copilot }: Values) {
+  function handleSubmit({ query: q, copilot, focus }: Values) {
     const params = new URLSearchParams({
       q,
       copilot: copilot.toString(),
+      focus,
     });
     popToRoot();
     open(`https://www.perplexity.ai/search?${params}`);
@@ -25,8 +27,20 @@ export default function Command() {
     >
       <Form.Description text="Where knowledge begins" />
       <Form.TextArea id="query" title="Ask anything..." autoFocus />
+      <Form.Dropdown
+        info="Focus allows you to fine tune your search by narrowing down the sources, for more targeted and relevant results. All follow-up questions in this Thread will focus the search on your chosen domain."
+        id="focus"
+        title="Focus"
+      >
+        <Form.Dropdown.Item value="all" title="All" />
+        <Form.Dropdown.Item value="scholar" title="Academic" />
+        <Form.Dropdown.Item value="writing" title="Writing" />
+        <Form.Dropdown.Item value="wolfram" title="Wolfram Alpha" />
+        <Form.Dropdown.Item value="youtube" title="Youtube" />
+        <Form.Dropdown.Item value="reddit" title="Reddit" />
+      </Form.Dropdown>
       <Form.Separator />
-      <Form.Checkbox defaultValue={false} id="copilot" title="Checkbox" label="Co-pilot" storeValue />
+      <Form.Checkbox defaultValue={false} id="copilot" label="Co-pilot" storeValue />
     </Form>
   );
 }
